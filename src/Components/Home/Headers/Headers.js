a612e995b4277cd35419cd933f6589941edf1eb2
rajonahmed749 <raazon749@gmail.com>
2021-04-26 09:08:44 +0600

    upload to gtihub

---
+++ src/Components/Home/Headers/Headers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+
+const Headers = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Headers;
---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Headers from '../Headers/Headers';
+
+const Home = () => {
+    return (
+        <div>
+            <Headers></Headers>
+        </div>
+    );
+};
+
+export default Home;
