235235e3e792d0df16dcd650d14faf3bee1981be
rajonahmed749 <raazon749@gmail.com>
2021-06-21 22:25:29 +0600

    hook form added

--- src/Components/Admin/AddService.js
+++ src/Components/Admin/AddService.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import "./Admin.css"
 
 const AddService = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();    
+
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+    const handleImage = e => {
+        console.log(e.target.files)
+    }
     return (
-        <div>
-            <h4>This is ad service section</h4>
+        <div className=" row justify-content-center">
+           <div className="col-md-5">
+           <form onSubmit={handleSubmit(onSubmit)} className="form-card">    
+                 <input type="text" name="Service" placeholder="Service" {...register("Service", { required: true })} />
+                
+                <input type="text" rows="5"  name="Description" placeholder="Description" {...register("Description", { required: true })} />
+
+              
+
+                <input onChange={handleImage} type="file" />
+              
+                               
+                <input type="submit" />
+            </form>
+           </div>
         </div>
     );
 };
